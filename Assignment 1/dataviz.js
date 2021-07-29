21205ad725feb87bdcd1e439ae6bd1323dd33c97
Matt Eland <Matt.Eland@GMail.com>
2021-07-28 22:56:53 -0400

    Global Temperature Data line fill

--- Assignment 1/dataviz.js
+++ Assignment 1/dataviz.js
@@ -1,6 +1,8 @@
 const nFill = "#ef8a62";
 const sFill = "#67a9cf";
-const margin = 50;
+const eFill = "#676767";
+const dataFill = "#fffacd";
+const margin = {top: 50, left: 50, right: 50, bottom: 100};
 let width = 800;
 let height = 500;
 
@@ -26,7 +28,7 @@ function drawHemispherePlot(svg, g, data) {
         .enter()
         .append("path")
         .attr('d', sHemData)
-        .attr("transform", d => "translate(" + (margin + x(d.Date)) + "," + (margin + y(d.SHem)) + ")")
+        .attr("transform", d => "translate(" + (margin.left + x(d.Date)) + "," + (margin.top + y(d.SHem)) + ")")
         .style("fill", sFill)
         .style("opacity", "0.7")
         .attr("stroke", "black")
@@ -44,7 +46,7 @@ function drawHemispherePlot(svg, g, data) {
         .enter()
         .append("path")
         .attr('d', nHemData)
-        .attr("transform", d => "translate(" + (margin + x(d.Date)) + "," + (margin + y(d.NHem)) + ")")
+        .attr("transform", d => "translate(" + (margin.left + x(d.Date)) + "," + (margin.top + y(d.NHem)) + ")")
         .style("fill", nFill)
         .style("opacity", "0.7")
         .attr("stroke", "black")
@@ -60,7 +62,7 @@ function drawHemispherePlot(svg, g, data) {
 
 function addTimeSeries(g) {
     const x = d3.scaleTime()
-        .domain([new Date('1/1/1875'), new Date('1/1/2015')])
+        .domain([new Date('1/1/1880'), new Date('1/1/2015')])
         .range([0, width]);
 
     g.append("g")
@@ -76,20 +78,51 @@ function drawLinePlot(svg, g, data) {
     console.debug(data.columns);
     const x = addTimeSeries(g);
 
+      svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top + 40) + ")")
+      .style("text-anchor", "middle")
+      .text("Year");
+
     const y = d3.scaleLinear()
         .domain([-250, 250])
         .range([height, 0]);
     
+        
     g.append("g").call(d3.axisLeft(y))
     
-    drawLineGraph(svg, data, x, y, d => d["64N-90N"], '#764F26');
-    drawLineGraph(svg, data, x, y, d => d["44N-64N"], '#916A2F');
-    drawLineGraph(svg, data, x, y, d => d["24N-44N"], '#AB883A');
-    drawLineGraph(svg, data, x, y, d => d["EQU-24N"], '#C2A847');
-    drawLineGraph(svg, data, x, y, d => d["24S-EQU"], '#D29ACD');
-    drawLineGraph(svg, data, x, y, d => d["44S-24S"], '#BA769B');
-    drawLineGraph(svg, data, x, y, d => d["64S-44S"], '#9A576E');
-    drawLineGraph(svg, data, x, y, d => d["90S-64S"], '#753D46');
+    //drawLineGraph(svg, data, x, y, d => d["64N-90N"], '#764F26');
+    //drawLineGraph(svg, data, x, y, d => d["44N-64N"], '#916A2F');
+    //drawLineGraph(svg, data, x, y, d => d["24N-44N"], '#AB883A');
+    //drawLineGraph(svg, data, x, y, d => d["EQU-24N"], '#C2A847');
+    //drawLineGraph(svg, data, x, y, d => d["24S-EQU"], '#D29ACD');
+    //drawLineGraph(svg, data, x, y, d => d["44S-24S"], '#BA769B');
+    //drawLineGraph(svg, data, x, y, d => d["64S-44S"], '#9A576E');
+    //drawLineGraph(svg, data, x, y, d => d["90S-64S"], '#753D46');
+    drawLineGraph(svg, data, x, y, d => d["Min"], sFill);
+    drawLineGraph(svg, data, x, y, d => d["Max"], nFill);
+    //drawLineGraph(svg, data, x, y, d => d["Glob"], eFill);
+
+		  	//Area
+        //var indexes = d3.range(data.length); 
+
+    //Area in between
+    svg.append("path")
+        .datum(data)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr('fill', dataFill)
+        .style('opacity', 0.5)
+        .attr("d", d3.area()
+            .x(function (d) {
+                return x(d.Date);
+            })
+            .y0(function (d) {
+                return y(d.Max);
+            })
+            .y1(function (d) {
+                return y(d.Min);
+            }));    
 }
 
 function drawLineGraph(svg, data, x, y, dataFunc, stroke) {
@@ -98,7 +131,7 @@ function drawLineGraph(svg, data, x, y, dataFunc, stroke) {
         .attr("fill", "none")
         .attr("stroke", stroke)
         .attr("stroke-width", 1.5)
-        .attr("transform", "translate(" + margin + "," + margin + ")")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
         .attr("d", d3.line()
             .x(function (d) { return x(d.Date); })
             .y(function (d) { return y(dataFunc(d)); })
@@ -112,11 +145,11 @@ function clearTooltip() {
 document.addEventListener('DOMContentLoaded', () => {
 
     const svg = d3.select("svg");
-    width = svg.attr("width") - margin - margin,
-    height = svg.attr("height") - margin - margin;
+    width = svg.attr("width") - margin.left - margin.right,
+    height = svg.attr("height") - margin.top - margin.bottom;
 
     const g = svg.append("g")
-        .attr("transform", "translate(" + margin + "," + margin + ")")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
     
     // Define the div for the tooltip
     tooltipDiv = d3.select("body").append("div")	
@@ -130,6 +163,8 @@ document.addEventListener('DOMContentLoaded', () => {
         row.SHem = +row.SHem;
         row.Glob = +row.Glob;
         row.Year = +row.Year;
+        row.Min = Math.min(+row["64N-90N"], +row["44N-64N"], +row["24N-44N"], +row["EQU-24N"], +row["24S-EQU"],  +row["44S-24S"], +row["64S-44S"], +row["90S-64S"])
+        row.Max = Math.max(+row["64N-90N"], +row["44N-64N"], +row["24N-44N"], +row["EQU-24N"], +row["24S-EQU"],  +row["44S-24S"], +row["64S-44S"], +row["90S-64S"])
 
         return row;
     })
